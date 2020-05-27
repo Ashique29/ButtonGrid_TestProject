1193409ff501855310ea11dc1dc9b25823725b32
MD. Assaduzzaman Ashique <ashique12301017@gmail.com>
2020-05-27 17:47:41 +1000

    Auto row generation functionality added.

--- TestPro01/Assets/Scripts/ButtonGrid/ButtonGridLayer.cs
+++ TestPro01/Assets/Scripts/ButtonGrid/ButtonGridLayer.cs
@@ -7,8 +7,11 @@ public class ButtonGridLayer : MonoBehaviour
 {
     public GameObject GridContainer;
 
-    public  int row = 2;
+    private  int row = 1;
     private int col;
+    private int objCounter = 0;
+    private bool isSmaller = false;
+    private int numberOfButton = 16;
     private int _imageWidth = 100;
     private int _imageHeight = 100;
     private int _offset = 10;
@@ -48,6 +51,7 @@ public class ButtonGridLayer : MonoBehaviour
         else
         {
             col = (int)GridContainer.GetComponent<RectTransform>().sizeDelta.x / (_imageWidth + _offset);
+            SetRow(col);
         }
     }
 
@@ -57,15 +61,24 @@ public class ButtonGridLayer : MonoBehaviour
         {
             for (int j = 0; j < col; j++)
             {
-                var GridObject = new GameObject();
-                GridObject.name = "Grid Conteiner";
-                GridObject.transform.SetParent(GridContainer.transform);
-                GridObject.AddComponent<RectTransform>();
-                GridObject.GetComponent<RectTransform>().pivot = new Vector2(1, 1);
-                GridObject.GetComponent<RectTransform>().sizeDelta = new Vector2(_imageWidth, _imageHeight);
-                GridObject.GetComponent<RectTransform>().position = new Vector3((GridContainer.GetComponent<RectTransform>().position.x - _offset - (j * (GridObject.GetComponent<RectTransform>().sizeDelta.x + _offset))), (GridContainer.GetComponent<RectTransform>().position.y - _offset - (i * (GridObject.GetComponent<RectTransform>().sizeDelta.y + _offset))), 1);
-                GridObject.AddComponent<Image>();
-                GridObject.GetComponent<Image>().color = myColor[j];
+                if (objCounter == 16)
+                {
+                    break;
+                }
+                else
+                {
+                    var GridObject = new GameObject();
+                    GridObject.name = "Grid Conteiner";
+                    GridObject.transform.SetParent(GridContainer.transform);
+                    GridObject.AddComponent<RectTransform>();
+                    GridObject.GetComponent<RectTransform>().pivot = new Vector2(1, 1);
+                    GridObject.GetComponent<RectTransform>().sizeDelta = new Vector2(_imageWidth, _imageHeight);
+                    GridObject.GetComponent<RectTransform>().position = new Vector3((GridContainer.GetComponent<RectTransform>().position.x - _offset - (j * (GridObject.GetComponent<RectTransform>().sizeDelta.x + _offset))), (GridContainer.GetComponent<RectTransform>().position.y - _offset - (i * (GridObject.GetComponent<RectTransform>().sizeDelta.y + _offset))), 1);
+                    GridObject.AddComponent<Image>();
+                    GridObject.GetComponent<Image>().color = myColor[j];
+                    objCounter++;
+                    Debug.Log(objCounter);
+                }
             }
         }
     }
@@ -81,5 +94,21 @@ public class ButtonGridLayer : MonoBehaviour
         myColor.Add(Color.green);
         myColor.Add(Color.cyan);
         myColor.Add(Color.blue);
+    }
+
+    void SetRow(int colNum)
+    {
+        while (!isSmaller)
+        {
+            if(numberOfButton > colNum)
+            {
+                row++;
+                numberOfButton = numberOfButton - colNum;
+            }
+            else
+            {
+                isSmaller = true;
+            }
+        }
     }
 }
